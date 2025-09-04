af6bd65cdf634eed93878bdf1abf0c720a829bdf
zzyygg <41406223+zzyygg@users.noreply.github.com>
2025-09-04 18:06:29 +0800

    Create NameCheckProcessor.java

---
+++ NameCheckProcessor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2025. All rights reserved.
+ */
+
+package com.huawei.itsc.system.service;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+/**
+ * 描述
+ *
+ * @author Guoxiujun
+ * @since 2025-09-04
+ */
+@SupportedAnnotationTypes("*")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class NameCheckProcessor extends AbstractProcessor {
+
+    private NameChecker nameChecker;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        nameChecker = new NameChecker(processingEnv);
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if (!roundEnv.processingOver()) {
+            for (Element typeElement : roundEnv.getRootElements()) {
+                nameChecker.checkNames(typeElement);
+            }
+        }
+        return false;
+    }
+}
